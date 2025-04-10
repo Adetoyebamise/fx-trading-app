897dfea01f926ffd9cabef2430ca28678af80b5f
Toye Babalola <adetoyebabalola@punch.agency>
2025-04-10 14:35:13 +0100

    nodemailer inclusion

---
+++ src/services/NodeMailer/service.ts
@@ -0,0 +1,34 @@
+import nodemailer from 'nodemailer';
+
+export class Nodemailer {
+  private transporter: any;
+  private maillist: string[];
+
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com ',
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS, //process.env.MAIL_PASS,
+      },
+    });
+    this.maillist = ['adetoyebamise@gmail.com'];
+  }
+
+  public async sendEmailToUser(subject: string): Promise<void> {
+    const mailOptions: nodemailer.SendMailOptions = {
+      from: '<adetoyebamise@gmail.com>',
+      to: this.maillist,
+      subject: 'FX trading',
+      html: `<b>Hey there! </b> <br><br> You've got a notification from  app <br><br> -  <b>${subject}</b>`,
+    };
+    try {
+      const info = await this.transporter.sendMail(mailOptions);
+      console.log('Email sent:', info.response);
+    } catch (error) {
+      console.error('Email sending failed:', error);
+    }
+  }
+}
